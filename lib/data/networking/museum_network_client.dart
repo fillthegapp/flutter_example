28e8c5dee25fa0a8dd6ee96fc17b9fde51ea6380
Emilio Villegas <emilio.villegas@fillthegapp.com>
2023-08-28 10:00:26 +0200

    - Add SSL pinning

--- lib/data/networking/museum_network_client.dart
+++ lib/data/networking/museum_network_client.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:flutter/services.dart';
 
 const Map<String, dynamic> _defaultQueryParameters = {'key': '0fiuZFh4'};
 
@@ -16,6 +19,16 @@ class MuseumNetworkClient {
     String path, {
     Map<String, dynamic>? queryParameters,
   }) async {
+    ByteData certificate =
+        await rootBundle.load('certificates/rijksmuseum.pem');
+
+    (_dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+      SecurityContext sc = SecurityContext();
+      sc.setTrustedCertificatesBytes(certificate.buffer.asUint8List());
+      HttpClient httpClient = HttpClient(context: sc);
+      return httpClient;
+    };
+
     return await _dio.get(
       path,
       queryParameters: Map.from(_defaultQueryParameters)
